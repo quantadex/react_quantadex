b7d5dc4003dddd114ab8b0ae7bea69837689daf5
TinyMolecule <lknguyen10@gmail.com>
2019-04-01 13:23:24 -0700

    remove last price in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -70,18 +70,18 @@ class Header extends Component {
 		return (
 			<div className={container}>
 				<div className="d-flex justify-content-between align-items-center">
-					<div className="d-flex">
+					<div className="w-100">
 						<Link to={"/" + this.props.network + "/exchange/" + (this.props.currentTicker ? this.props.currentTicker.replace("/", "_") : "")} className="header-logo">
 							<img src={this.props.network == "mainnet" ? devicePath("public/images/logo-light.svg") : devicePath("public/images/qdex-fantasy-light.svg")} width="220" />
 						</Link>
 						
 					</div>
-					<div className="price-stats d-flex">
+					{/* <div className="price-stats d-flex">
 						<div>
 							<label>Last Price</label><br/>
 							<span className="value">{this.props.currentPrice}</span>
 						</div>
-					</div>
+					</div> */}
 
 					{this.props.network == "testnet" ?
 						<div className="leaderboard-link">
