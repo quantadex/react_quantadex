6ecbc02b06f6c3d1ca6ebfea3dbb96190fb002c6
Quoc <quocble@gmail.com>
2019-04-21 02:14:54 -0700

    new build config

--- mobile_app/platforms/android/platform_www/cordova_plugins.js
+++ mobile_app/platforms/android/platform_www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
-        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
-        "pluginId": "phonegap-plugin-barcodescanner",
-        "clobbers": [
-            "cordova.plugins.barcodeScanner"
-        ]
-    },
-    {
         "id": "cordova-plugin-ionic-keyboard.keyboard",
         "file": "plugins/cordova-plugin-ionic-keyboard/www/android/keyboard.js",
         "pluginId": "cordova-plugin-ionic-keyboard",
@@ -41,18 +33,26 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
+    },
+    {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-compat": "1.2.1-dev",
-    "cordova-plugin-whitelist": "1.3.3",
-    "phonegap-plugin-barcodescanner": "6.0.8",
-    "cordova-plugin-remote-injection": "0.5.2",
     "cordova-plugin-ionic-keyboard": "2.1.3",
     "cordova-plugin-nativestorage": "2.3.2",
-    "cordova-plugin-splashscreen": "5.0.2"
+    "cordova-plugin-remote-injection": "0.5.2",
+    "cordova-plugin-splashscreen": "5.0.2",
+    "cordova-plugin-whitelist": "1.3.3",
+    "phonegap-plugin-barcodescanner": "6.0.8"
 };
 // BOTTOM OF METADATA
 });
