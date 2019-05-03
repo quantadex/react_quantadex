5a5d625dc96229b3167964345cba6d906b6b7a44
TinyMolecule <lknguyen10@gmail.com>
2019-05-03 14:14:07 -0700

    autocomplete

--- src/components/ui/searchBox.jsx
+++ src/components/ui/searchBox.jsx
@@ -11,10 +11,9 @@ const container = css `
 `
 
 export default class SearchBox extends Component {
-    // really silly way to remove the autocomplete on chrome
     render() {
         return (
-            <input className={container} spellCheck="false" autoComplete="nope" onClick={(e) => e.stopPropagation()}
+            <input className={container} spellCheck="false" autoComplete="off" onClick={(e) => e.stopPropagation()}
 				onChange={this.props.onChange} placeholder={this.props.placeholder} style={this.props.style} />
         )
     }
